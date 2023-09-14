5f0da34867da29a5eb862cb5a14dc9e29a26a036
sophirv05 <114093203+sophirv05@users.noreply.github.com>
2023-09-13 21:39:05 -0400

    Week1 Name and Major

---
+++ NameAndMajor.java
@@ -0,0 +1,7 @@
+package cs176l;
+
+public class NameAndMajor {
+	public static void main(String[] args) {
+	System.out.println("Hi! My name is Sophia and my major is Software Engineering with a minor in Math and Computer Science");
+	}
+}
