edefa3e0aab5a5e4cffbe0624ce362b0030c3c7d
sophirv05 <114093203+sophirv05@users.noreply.github.com>
2023-09-13 21:36:29 -0400

    Week2 Problem1

---
+++ Week2Problem1.java
@@ -0,0 +1,9 @@
+package cs176l;
+
+public class Week2Problem1 {
+	public static void main(String[] args) {
+		System.out.println("\\(>_<)");
+		System.out.println("  ))z");
+		System.out.println("  / \\");
+	}
+}
